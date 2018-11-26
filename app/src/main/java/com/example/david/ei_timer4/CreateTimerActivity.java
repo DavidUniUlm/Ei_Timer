744ead22e05b1a5d68cdc0baf89fd5588bb668ef
David <david.renner@uni-ulm.de>
2018-11-26 12:16:14 +0100

    numberPicker

--- app/src/main/java/com/example/david/ei_timer4/CreateTimerActivity.java
+++ app/src/main/java/com/example/david/ei_timer4/CreateTimerActivity.java
@@ -10,12 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
 
 import java.io.IOException;
 
 public class CreateTimerActivity extends AppCompatActivity {
 
-    //Button takePhotoButton;
+    NumberPicker numberPicker_Hour;
+    NumberPicker numberPicker_Min;
+    NumberPicker numberPicker_Sec;
+
     ImageView imageView;
     private static final int PICK_FROM_CAMERA = 0;
     private static final int PICK_FROM_FILE = 1;
@@ -28,19 +32,52 @@ public class CreateTimerActivity extends AppCompatActivity {
 
         //takePhotoButton = (Button)findViewById(R.id.takePhotoButton);
         imageView = (ImageView) findViewById(R.id.imageView);
+
+        initializeNumberPicker();
     }
 
-    public void onTakePhotoButtonClick(View view) {
+    private void initializeNumberPicker() {
+
+        numberPicker_Hour = findViewById(R.id.hour);
+        numberPicker_Min = findViewById(R.id.min);
+        numberPicker_Sec = findViewById(R.id.sec);
+
+        numberPicker_Hour.setMaxValue(99);
+        numberPicker_Min.setMaxValue(59);
+        numberPicker_Sec.setMaxValue(59);
+        
+        numberPicker_Hour.setMinValue(0);
+        numberPicker_Min.setMinValue(0);
+        numberPicker_Sec.setMinValue(0);
+    }
+
+    public void onPhotoButtonClick(View view) {
         //Achtung: Stürzt ab, wenn das Foto verworfen wird
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(intent, PICK_FROM_CAMERA);
+
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, PICK_FROM_CAMERA);
+        Intent intent2 = new Intent(Intent.ACTION_PICK);
+        intent2.setType("image/*");
+        intent2.setData(MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        intent2.putExtra("return-data", true);
+
+        Intent intent3 = Intent.createChooser(intent, "sdjfsal");
+        ((Intent) intent3).putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{
+                intent2
+        });
+
+        startActivityForResult(intent3, PICK_FROM_CAMERA);
+
+
     }
 
     public void onChoosePhotoFromGalleryButtonClick(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Choose Picture"), PICK_FROM_FILE);
+//        Intent intent = new Intent();
+//        intent.setType("image/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityForResult(Intent.createChooser(intent, "Choose Picture"), PICK_FROM_FILE);
     }
 
     public void onChooseRingtoneButtonClick(View view) {
@@ -54,10 +91,15 @@ public class CreateTimerActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case PICK_FROM_CAMERA:
-                    System.out.println(data.getData());
-                    Bitmap picFromCam = (Bitmap) data.getExtras().get("data");
-                    imageView.setImageBitmap(picFromCam);
+                    Uri imageFromGallery = data.getData();
+                    if (imageFromGallery == null) {
+                        Bitmap picFromCam = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(picFromCam);
+                    } else {
+                        imageView.setImageURI(imageFromGallery);
+                    }
                     break;
+
 
                 case PICK_FROM_FILE:
                     Uri image = data.getData();
