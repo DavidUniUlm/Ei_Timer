0601c3f37b0b368f6fdda6624a57472de8c4c561
David Renner <david.renner@uni-ulm.de>
2018-12-01 11:52:08 +0100

    timer läuft, kleiner bug vorhanden

--- app/src/main/java/com/example/david/ei_timer4/TimerListAdapter.java
+++ app/src/main/java/com/example/david/ei_timer4/TimerListAdapter.java
@@ -3,10 +3,8 @@ package com.example.david.ei_timer4;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +20,6 @@ public class TimerListAdapter extends ArrayAdapter<Timer> {
 
     private Context mContext;
     int mResource;
-    //private Sparse s = new SparseArray<>()
-    HashMap<View, CountDownTimer> hashMap = new HashMap<>();
 
 
     public TimerListAdapter(Context context, int resource, ArrayList<Timer> objects) {
@@ -36,12 +32,6 @@ public class TimerListAdapter extends ArrayAdapter<Timer> {
     @Override
     public View getView(final int position, View convertView, final ViewGroup parent) {
 
-        final String name = getItem(position).getName();
-        long time = getItem(position).getTime();
-        final Uri picture = getItem(position).getPicture();
-        final Uri ringtone = getItem(position).getRingtone();
-        final boolean isRunning = getItem(position).isRunning();
-
         final LayoutInflater inflater = LayoutInflater.from(mContext);
         convertView = inflater.inflate(mResource, parent, false);
 
@@ -49,71 +39,56 @@ public class TimerListAdapter extends ArrayAdapter<Timer> {
         TextView nameTv = convertView.findViewById(R.id.nameTimerLayout);
         final TextView countdownTv = convertView.findViewById(R.id.countdownTimerLayout);
 
-        iv.setImageURI(picture);
-        nameTv.setText(name);
-        countdownTv.setText(millisToString(time));
-
-//        CountDownTimer countDownTimer = new CountDownTimer(time, 1000) {
-//            @Override
-//            public void onTick(long millisUntilFinished) {
-//                countdownTv.setText(millisToString(millisUntilFinished));
-//                getItem(position).setTime(millisUntilFinished);
-//            }
-//
-//            @Override
-//            public void onFinish() {
-//                getItem(position).setTime(0);
-//                getItem(position).setRunning(false);
-//                Intent intent = new Intent(mContext, TimerFinished.class);
-//                intent.putExtra("name", name);
-//                intent.putExtra("picture", picture);
-//                intent.putExtra("ringtone", ringtone);
-//                mContext.startActivity(intent);
-//            }
-//        };
-
-//        if (time != 0 && isRunning) {
-//            countDownTimer.start();
-//        }
-
-        hashMap.put(convertView, null);
+        iv.setImageURI(getItem(position).getPicture());
+        nameTv.setText(getItem(position).getName());
+        countdownTv.setText(millisToString(getItem(position).getTime()));
 
         convertView.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
+
+            CountDownTimer countDownTimer;
+
             @Override
             public void onClick(View v) {
-//                if (getItem(position).getTime() != 0) {
-                    if(getItem(position).isRunning()){
-                        hashMap.get(v).cancel();
-                    }
-                    else{
-                        //getItem(position).setRunning(true);
-                        hashMap.replace(v, new CountDownTimer(getItem(position).getTime(), 1000) {
-                            @Override
-                            public void onTick(long millisUntilFinished) {
-                                countdownTv.setText(millisToString(millisUntilFinished));
-                                getItem(position).setTime(millisUntilFinished);
-                            }
 
-                            @Override
-                            public void onFinish() {
-                                getItem(position).setTime(0);
-                                getItem(position).setRunning(false);
-                                Intent intent = new Intent(mContext, TimerFinished.class);
-                                intent.putExtra("name", name);
-                                intent.putExtra("picture", picture);
-                                intent.putExtra("ringtone", ringtone);
-                                mContext.startActivity(intent);
-                            }
-                        }.start());
+//                TODO: Bug
+//                Timer 1 starten
+//                Neuen timer hinzufügen
+//                Timer 1 anhalten – wieder starten und die zeit springt
 
+                if (getItem(position).isRunning() && countDownTimer != null) {
+                    countDownTimer.cancel();
+                    //getItem(position).setRunning(false);
+                } else {
+                    countDownTimer = new CountDownTimer(getItem(position).getTime(), 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            countdownTv.setText(millisToString(millisUntilFinished));
+                            getItem(position).setTime(millisUntilFinished);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            getItem(position).setTime(0);
+                            getItem(position).setRunning(false);
+                            Intent intent = new Intent(mContext, TimerFinished.class);
+                            intent.putExtra("name", getItem(position).getName());
+                            intent.putExtra("picture", getItem(position).getPicture());
+                            intent.putExtra("ringtone", getItem(position).getRingtone());
+                            mContext.startActivity(intent);
+                        }
+                    };
+                    countDownTimer.start();
+                    //getItem(position).setRunning(true);
                 }
-                getItem(position).setRunning(!(getItem(position).isRunning()));
+                getItem(position).setRunning(!getItem(position).isRunning());
             }
         });
-
+        if (getItem(position).isRunning()) {
+            convertView.callOnClick();
+        }
         return convertView;
     }
+
 
     private String millisToString(long millis) {
         int hour = (int) millis / 1000 / 3600;
